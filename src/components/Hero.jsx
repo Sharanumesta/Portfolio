829bb89ca5b9813a824522adabc7935b412a9802
Sharanu Mesta <sharanumesta1201@gmail.com>
2026-01-25 22:23:09 +0530

    Updated resume download link in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,7 +86,7 @@ const Hero = () => {
                   : "border-purple-600 hover:bg-purple-600/10 text-purple-700 shadow-purple-400/10"
               }`}
             >
-              <a href="https://drive.google.com/file/d/1W-L0lsvoNRzmdgS7Fsh-XWCaX-ePBqaO/view?usp=drive_link" target="_blank">Download Resume</a>
+              <a href="https://drive.google.com/file/d/1s7vmSl1phnMu7E4stbOXsmRqCCMU5Uwc/view?usp=drive_link" target="_blank">Download Resume</a>
             </motion.button>
           </motion.div>
         </div>
